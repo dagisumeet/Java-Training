63f46e6b9fc03ba1d90dbd9f55ee06d1af223dcf
unknown <sumeet.dagi@gmail.com>
2022-02-07 04:36:59 -0600

    Learning states & Behaviors

---
+++ src/Java/Training/Practice/Day1/Bank.java
@@ -0,0 +1,21 @@
+package Java.Training.Practice.Day1;
+
+public class Bank {
+}
+
+//states
+
+//location
+//name
+//category
+//accounts
+//employee
+//customers
+
+//behavior
+//deposits
+//withdraws
+//loan
+//auction
+//transfer
+//locker
---
+++ src/Java/Training/Practice/Day1/Television.java
@@ -0,0 +1,23 @@
+package Java.Training.Practice.Day1;
+
+public class Television {
+}
+
+//states
+
+//brand
+//size
+//screen
+//version
+//price
+//color
+
+//behaviors
+
+//displays
+//sounds
+//lights up
+//connects to internet
+//shuts off
+
+
