7d01c2750a5de888c796812ce6de25640a2c7993
unknown <sumeet.dagi@gmail.com>
2022-02-09 06:15:11 -0600

    PracticeDataFunction

---
+++ src/Java/Training/Practice/Day1/javatypepractice.java
@@ -0,0 +1,25 @@
+package Java.Training.Practice.Day1;
+
+public class javatypepractice {
+public static void main (String [] args)  {
+
+    welcome();
+    multiply(10, 10);
+    multiply(20, 10);
+    multiply(20, 20);
+    devide(9, 3);
+   devide(80, 5);
+    devide(18,9);
+    }
+    public static void welcome() {
+        System.out.println("Welcome to our guest");
+    }
+
+    public static void multiply(int a, int b) {
+    System.out.println(a * b);
+    }
+    public static void devide(int a, int b) {
+        System.out.println(a / b);
+    }
+
+}
